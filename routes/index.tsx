1bc615d8af2bbdd6a7634278aa74e7c83ea00bda
jhonatan <jhonatan.s.costa97@gmail.com>
2023-07-27 10:55:14 -0300

    feat(app): rename project

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>pingboard</title>
+        <title>Org chat</title>
       </Head>
       <div class="px-4 py-8 mx-auto bg-[#86efac]">
-          <h1>Pingboard</h1>
+          <h1>Org chat</h1>
       </div>
     </>
   );
