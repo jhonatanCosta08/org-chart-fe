94802c4628f5750fd11f2857f10aecaa656c6f4b
jhonatan <jhonatan.s.costa97@gmail.com>
2023-08-02 15:00:09 -0300

    feat(position): add position styles

--- islands/position.tsx
+++ islands/position.tsx
@@ -5,23 +5,52 @@ interface PositionModel {
         id: string,
         name: string,
         created_at: string,
-        person: []
+        person: [],
+        positionColor: string
     }]
 }
 
 const positionStyles = {
     positionContainer: {
-        width: '25%'
+        width: '25%',
+        opacity: '1'
+    },
+    positionBox: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem',
+        margin: '0 0.5rem 1.5rem 0.5rem',
+        borderRadius: '5px'
+    },
+    textStyles: {
+        fontSize: '1rem',
+        letterSpacing: '1px',
+        textTransform: 'capitalize',
+        fontWeight: 600
     }
+}
+
+function hexToRgba(hex) {
+    let rgbColor;
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    rgbColor = result ? {r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16),} : null;
+    return `rgba(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}, ${0.4})`;
 }
 
 
 export default function Position(props: PositionModel) {
+    console.log('pos: ', props);
     return (
         <>
             {props.position.map((item) => (
                 <div className='position-container' style={positionStyles.positionContainer}>
-                    <p>{item.name}</p>
+                    <div class='position-box' style={{ ...positionStyles.positionBox, backgroundColor: hexToRgba(item.positionColor), borderTop: `2px solid ${item.positionColor}` }}>
+                        <p style={positionStyles.textStyles}>{item.name}</p>
+                        <p style={positionStyles.textStyles}>{item.person.length}</p>
+                    </div>
                     <Person person={item.person}/>
                 </div>
             ))}
