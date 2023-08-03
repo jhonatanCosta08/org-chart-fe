3d6fed60f014333fcc81e9a7814d19f08403366a
jhonatan <jhonatan.s.costa97@gmail.com>
2023-08-03 14:45:59 -0300

    feat(app): change time to sub request

--- routes/person/[id].ts
+++ routes/person/[id].ts
@@ -3,6 +3,6 @@ import {supabaseAdmin} from "../../lib/supabase.ts";
 
 export const handler = async (_req: Request, _ctx: HandlerContext): Response => {
     const person = await supabaseAdmin.from('person').select().eq('id', _ctx.params.id).single().select('*, person(*, person(*))');
-    await new Promise(resolve => setTimeout(resolve, 200));
+    await new Promise(resolve => setTimeout(resolve, 30));
     return new Response(JSON.stringify(person.data));
 };
