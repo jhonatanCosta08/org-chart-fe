806befbc5ce2843e1a6cb90835984be2288da5ad
jhonatan <jhonatan.s.costa97@gmail.com>
2023-08-03 13:40:08 -0300

    feat(app): add error check in color function and remove unused method in people

--- api/people.ts
+++ api/people.ts
@@ -1,15 +1,4 @@
 
-export async function getAllPersons() {
-    const url = 'http://localhost:4200/person';
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (error) {
-        console.error('Ocorreu um erro:', error);
-        throw error;
-    }
-}
-
 export async function getPersonById(id: string) {
     const url = `http://localhost:4200/person/${id}`;
     try {
--- islands/position.tsx
+++ islands/position.tsx
@@ -32,12 +32,16 @@ const positionStyles = {
 }
 
 function hexToRgba(hex) {
-    let rgbColor;
-    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    rgbColor = result ? {r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16),} : null;
-    return `rgba(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}, ${0.4})`;
+    if(!hex.includes('rgb')) {
+        let rgbColor;
+        const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        rgbColor = result ? {r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16),} : null;
+        return `rgba(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}, ${0.4})`;
+    }else {
+        return hex;
+    }
 }
 
 
