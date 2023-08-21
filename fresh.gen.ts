71b9fadaab61dbece385e588bdeaf78db25de299
jhonatan <jhonatan.s.costa97@gmail.com>
2023-08-21 14:53:46 -0300

    feat(login): start working in sign in and up login

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,10 +7,11 @@ import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/dashboard.tsx";
 import * as $3 from "./routes/index.tsx";
 import * as $4 from "./routes/login.tsx";
-import * as $5 from "./routes/person/[id].ts";
-import * as $6 from "./routes/person/index.ts";
-import * as $7 from "./routes/position/[id].ts";
-import * as $8 from "./routes/position/index.ts";
+import * as $5 from "./routes/auth/index.ts";
+import * as $6 from "./routes/person/[id].ts";
+import * as $7 from "./routes/person/index.ts";
+import * as $8 from "./routes/position/[id].ts";
+import * as $9 from "./routes/position/index.ts";
 import * as $$0 from "./islands/drawer/drawer-styles.ts";
 import * as $$1 from "./islands/drawer/drawer.tsx";
 import * as $$2 from "./islands/login-island/login-island.tsx";
@@ -29,10 +30,11 @@ const manifest = {
     "./routes/dashboard.tsx": $2,
     "./routes/index.tsx": $3,
     "./routes/login.tsx": $4,
-    "./routes/person/[id].ts": $5,
-    "./routes/person/index.ts": $6,
-    "./routes/position/[id].ts": $7,
-    "./routes/position/index.ts": $8,
+    "./routes/login/index.ts": $5,
+    "./routes/person/[id].ts": $6,
+    "./routes/person/index.ts": $7,
+    "./routes/position/[id].ts": $8,
+    "./routes/position/index.ts": $9,
   },
   islands: {
     "./islands/drawer/drawer-styles.ts": $$0,
